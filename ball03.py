3a216bcbc892245c5bda51f40bb8f8ee579a12e7
hugo <hfgarcia00@gmail.com>
2021-04-28 22:12:47 +0200

    moviendo bolas

--- ball03.py
+++ ball03.py
@@ -3,16 +3,6 @@ import sys
 
 from random import randint
 
-def rebotaX(x):
-    if x <= 0 or x >=ANCHO:
-        return -1
-    return 1
-
-def rebotaY(y):
-    if y <= 0 or y >= ALTO:
-        return -1
-    return 1
-
 ROJO = (255,0,0)
 AZUL = (0,0,255)
 VERDE = (0,255,0)
@@ -25,20 +15,6 @@ pg.init()
 pantalla = pg.display.set_mode((ANCHO,ALTO))
 
 gameOver = False
-
-
-# Bola 01
-xB = ANCHO // 2
-yB = ALTO // 2
-xvB = -5
-yvB = -5
-
-# Bola 02
-
-xB2 = randint(0, ANCHO)
-yB2 = randint(0, ALTO)
-xvB2 = randint(5, 15)
-yvB2 = randint(5, 15)
 
 reloj = pg.time.Clock()
 
@@ -55,13 +31,16 @@ class Bola():
         self.x += self.vx
         self.y += self.vy
 
-        if self.y <= 0 or self.y >= alto:
+        if (self.y - self.radio) <= 0 or (self.y + self.radio) >= alto:
             self.vy *= -1
-        if self.x <= 0 or self.x >= ancho:
+            """
+            if self.color != NEGRO:
+                self.color = NEGRO
+            else:
+                self.color = (randint(0, 255), randint(0, 255),randint(0, 255))
+            """
+        if (self.x - self.radio) <= 0 or (self.x + self.radio) >= ancho:
             self.vx *= -1
-
-
-
 
 bolas = []
 for _ in range(10):
@@ -84,34 +63,10 @@ while not gameOver:
     for bola in bolas:
         bola.Rebota(ANCHO,ALTO)
 
-    """
-        bola.x += bola.vx
-        bola.y += bola.vy
-
-        bola.vx *= rebotaX(bola.x)
-        bola.vy *= rebotaY(bola.y)
-    """
     pantalla.fill(NEGRO)
     for bola in bolas:
         pg.draw.circle(pantalla, bola.color , (bola.x,bola.y), bola.radio)
 
-    """
-        #Movimiento bola 1
-    
-    xB += xvB
-    yB += yvB
-    xB2 += xvB2
-    yB2 += yvB2
-
-    xvB *= rebotaX(xB)
-    yvB *= rebotaY(yB)
-
-    xvB2 *= rebotaX(xB2)
-    yvB2 *= rebotaY(yB2)
-
-    pg.draw.circle(pantalla,ROJO , (xB,yB),10)
-    pg.draw.circle(pantalla,AZUL , (xB2,yB2),10)
-    """
 
     #Refrescar pantalla
 
